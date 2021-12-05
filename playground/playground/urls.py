2839d0b08c061efa9e0585fc5df5eeee2badb81f
Sid Hayoun Lee <sidhlee@gmail.com>
2021-12-05 17:41:08 -0500

    Register viewsets to router and include it under path

--- playground/playground/urls.py
+++ playground/playground/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from music import views
+
+music_router = routers.DefaultRouter()
+music_router.register(r"instruments", views.InstrumentViewSet)
+music_router.register(r"musicians", views.MusicianViewSet)
+music_router.register(r"genres", views.GenreViewSet)
+music_router.register(r"albums", views.AlbumViewSet)
+music_router.register(r"tracks", views.TrackViewSet)
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("music/", include(music_router.urls)),
+    path("admin/", admin.site.urls),
 ]
